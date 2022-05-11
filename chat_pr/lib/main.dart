ad1e90bd26a921d4d8f9f9329029aa3c9b9c53c9
duckddud213 <duckddud213@naver.com>
2022-05-11 18:15:23 +0900

    map page

--- chat_pr/lib/main.dart
+++ chat_pr/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:chat_pr/chat_page.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main(){
   runApp(Home());
@@ -10,6 +11,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: '마이아파',
       theme: ThemeData(
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -25,25 +27,67 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage> {
+  GoogleMapController? mapController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text("마이아파"),
+        title: Text("Second Route"),
       ),
-      body: Center(
-        child: OutlinedButton(
-          child: Text("채팅창 열기"),
-          style: OutlinedButton.styleFrom(primary: Colors.green),
-          onPressed: () {
-            Press_Chat(context);
-          },
-        ),
-      ),
+      body: FutureBuilder(
+        future: checkPermission(),
+        builder: (BuildContext context,AsyncSnapshot snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if(snapshot.data=='위치 권한이 허가되었습니다.'){
+            return Center(
+                child: OutlinedButton(
+                  child: Text("채팅창 열기"),
+                  style: OutlinedButton.styleFrom(primary: Colors.green),
+                  onPressed: () {
+                    Press_Chat(context);
+                  },
+                )
+            );
+          }
+
+          return Center(
+            child: Text(snapshot.data),
+          );
+        },
+      )
     );
   }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!isLocationEnabled){
+      return '위치 서비스를 활성화해주세요.';
+    }
+
+    LocationPermission  checkedPermission = await Geolocator.checkPermission();
+
+    if(checkedPermission == LocationPermission.denied){
+      checkedPermission = await Geolocator.requestPermission();
+
+      if(checkedPermission == LocationPermission.denied){
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if(checkedPermission == LocationPermission.deniedForever){
+      return '설정에서 앱의 위치 권한을 허가해주세요.';
+    }
+    print('check Permission successed');
+    return '위치 권한이 허가되었습니다.';
+  }
 }
+
+
 
 void Press_Chat(BuildContext context) {
   Navigator.push(
--- chat_pr/lib/map_page.dart
+++ chat_pr/lib/map_page.dart
@@ -1,28 +1,184 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void Press_Map(BuildContext context, String text) {
+void Press_Map(BuildContext context, String text) async{
   Navigator.push(
     context,
     MaterialPageRoute(
-      builder: (context) => SecondRoute(text: text),
+      builder: (context) => googleMap(),
     ),
   );
 }
 
-class SecondRoute extends StatelessWidget {
-  SecondRoute({required this.text});
+class googleMap extends StatefulWidget {
+  const googleMap({Key? key}) : super(key: key);
 
-  final String text;
+  @override
+  State<googleMap> createState() => _googleMapState();
+}
+
+class _googleMapState extends State<googleMap> {
+  GoogleMapController? mapController;
+
+  static LatLng initialLocation = LatLng(
+      37.5023273,
+      126.764252
+  );
+
+  static CameraPosition initialPosition = CameraPosition(
+    target: initialLocation,
+    zoom: 17,
+  );
+
+  static final double distance = 100;
+  static Circle circle = Circle(
+    circleId: CircleId('circle'),
+    center: initialLocation,
+    fillColor: Colors.blue.withOpacity(0.5),
+    radius: distance,
+    strokeColor: Colors.blue,
+    strokeWidth: 1,
+  );
+
+  static Marker marker = Marker(
+    markerId: MarkerId('marker'),
+    position: initialLocation,
+  );
+
+  void getLocation(LatLng initialLocation,CameraPosition initialPosition) async{
+    final location = await Geolocator.getCurrentPosition();
+
+    mapController!.animateCamera(CameraUpdate.newLatLng(
+      LatLng(
+        location.latitude,
+        location.longitude,
+      ),
+    )
+    );
+
+    initialLocation=LatLng(location.latitude,location.longitude);
+    initialPosition = CameraPosition(
+      target: LatLng(location.latitude,location.longitude),
+      zoom: 55,
+    );
+    return ;
+    }
+
+  @override
+  void initState(){
+    super.initState();
+    setState(() {
+      getLocation(initialLocation,initialPosition);
+      print('initState part');
+      print(initialLocation);
+      print(initialPosition);
+    });
+  }
+
+  Future<String> moveLocation() async{
+    if(mapController == null){
+      return '에러';
+    }
+
+    final location = await Geolocator.getCurrentPosition();
+
+    mapController!.animateCamera(CameraUpdate.newLatLng(
+      LatLng(
+        location.latitude,
+        location.longitude,
+      ),
+    )
+    );
+
+    initialLocation=LatLng(location.latitude,location.longitude);
+    initialPosition = CameraPosition(
+      target: initialLocation,
+      zoom: 25,
+    );
+
+    return '현재 위치 확인 완료';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-          child: Text('$text'),
+        appBar: renderAppBar(),
+        body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: initialPosition,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
+        circles: Set.from([circle]),
+        markers: Set.from([marker]),
+        onMapCreated: onMapCreated,
+    )
+    );
+  }
+
+  onMapCreated(GoogleMapController controller){
+    mapController = controller;
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!isLocationEnabled){
+      return '위치 서비스를 활성화해주세요.';
+    }
+
+    LocationPermission  checkedPermission = await Geolocator.checkPermission();
+
+    if(checkedPermission == LocationPermission.denied){
+      checkedPermission = await Geolocator.requestPermission();
+
+      if(checkedPermission == LocationPermission.denied){
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if(checkedPermission == LocationPermission.deniedForever){
+      return '설정에서 앱의 위치 권한을 허가해주세요.';
+    }
+    print('check Permission successed');
+    return '위치 권한이 허가되었습니다.';
+  }
+
+  AppBar renderAppBar(){
+    return AppBar(
+      title: Text(
+        'Googlemap',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
         ),
+      ),
+      backgroundColor: Colors.white,
+      actions: [
+        IconButton(
+          onPressed: () async {
+            if(mapController == null){
+              return;
+            }
+            final location = await Geolocator.getCurrentPosition();
+
+            mapController!.animateCamera(CameraUpdate.newLatLng(
+              LatLng(
+                location.latitude,
+                location.longitude,
+              ),
+            )
+            );
+
+          },
+          color: Colors.blue,
+          icon: Icon(
+              Icons.my_location
+          ),
+        )
+      ],
     );
   }
 }
